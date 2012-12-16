040d4005dd041adff01bf8cbaec60258577df8c3
Dmitry Vakhrushev <self@kr41.net>
2012-12-16 16:05:13 +0700

    Added ProcessingTree to public names

--- configtree/__init__.py
+++ configtree/__init__.py
@@ -1,8 +1,8 @@
 from .tree import Tree, flatten
-from .config import ConfigTree
+from .config import ProcessingTree, ConfigTree
 
 
-__all__ = ['ConfigTree', 'Tree', 'flatten']
+__all__ = ['ConfigTree', 'ProcessingTree', 'Tree', 'flatten']
 __version__ = '0.1'
 __author__ = 'Dmitry Vakhrushev <self@kr41.net>'
 __license__ = 'BSD'
