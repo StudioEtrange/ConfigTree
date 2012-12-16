583e6279d27ee6cc6d6f72a6c185fb483c947ae5
Dmitry Vakhrushev <self@kr41.net>
2012-12-17 01:05:35 +0700

    Cleanup ConfigTree

--- configtree/config.py
+++ configtree/config.py
@@ -7,12 +7,6 @@ from .compat import string
 from .loader import load_json, load_yaml
 
 
-loaders = {
-    '.json': load_json,
-    '.yaml': load_yaml,
-}
-
-
 class ProcessingTree(Tree):
     """
     A Processing Tree is sublcass of :class:`Tree` which able to process
@@ -23,7 +17,7 @@ class ProcessingTree(Tree):
     ..  codeblock-pycon::
 
         >>> pt = ProcessingTree({'a.b.c': [1, 2, 3]})
-        >>> pt.update({'a.b.c#extend': [4, 5, 6]})
+        >>> pt['a.b.c#extend'] = [4, 5, 6]
         >>> pt
         ProcessingTree({'a.b.c': [1, 2, 3, 4, 5, 6]})
 
@@ -32,7 +26,7 @@ class ProcessingTree(Tree):
     ..  codeblock-pycon::
 
         >>> pt = ProcessingTree()
-        >>> pt.update({'a.b.c': '>>> 1 + 1'})
+        >>> pt['a.b.c'] = '>>> 1 + 1'
         >>> pt['a.b.c']
         2
         >>> pt['a.b.d'] = ">>> self['a.b.c'] * 2"
@@ -42,24 +36,24 @@ class ProcessingTree(Tree):
         >>> pt['a.b.e']
         6
 
-    Update ``__locals__`` branch to extend expression namespace:
+    Update ``__namespace__`` branch to extend names available in expression:
 
     ..  codeblock-pycon::
 
-        >>> from datetime import date
-        >>> pt['__locals__.date'] = date
-        >>> pt['today'] = '>>> date.today()'
-        >>> pt['today'] == date.today()
-        True
+        >>> from math import floor
+        >>> pt['__namespace__.floor'] = floor
+        >>> pt['x'] = '>>> int(floor(3.8))'
+        >>> pt['x']
+        3
 
-    Any string passed to ``__locals__`` branch will be treated as name
+    Any string passed to ``__namespace__`` branch will be treated as name
     to import:
 
     ..  codeblock-pycon::
 
-        >>> pt['__locals__.ceil'] = 'math:ceil'
-        >>> pt['x'] = '>>> int(ceil(3.1))'
-        >>> pt['x']
+        >>> pt['__namespace__.ceil'] = 'math:ceil'
+        >>> pt['y'] = '>>> int(ceil(3.1))'
+        >>> pt['y']
         4
 
     """
@@ -70,14 +64,18 @@ class ProcessingTree(Tree):
     def __setitem__(self, key, value):
         if isinstance(value, string):
             if value.startswith(self._exp_prefix):
-                branch = None
                 if self._key_sep in key:
                     branch_key = key.rsplit(self._key_sep, 1)[0]
                     branch = self.branch(branch_key)
+                else:
+                    branch = self
                 value = value[len(self._exp_prefix):]
-                value = eval(value, {'self': self, 'branch': branch},
-                                     self.branch('__locals__'))
-            elif key.startswith('__locals__' + self._key_sep):
+                value = eval(
+                    value,
+                    {'self': self, 'branch': branch},
+                    self.branch('__namespace__')
+                )
+            elif key.startswith('__namespace__' + self._key_sep):
                 value = EntryPoint.parse('x={0}'.format(value)).load(False)
         if self._method_sep in key:
             key, method = key.split(self._method_sep)
@@ -93,37 +91,48 @@ class ConfigTree(ProcessingTree):
 
     """
 
+    loaders = {
+        '.json': load_json,
+        '.yaml': load_yaml,
+    }
+
     def load(self, path):
-        self.include(path)
-        queue = self['__loader__.queue']
+        queue = deque()
+
+        def abspath(relpath):
+            if os.path.isabs(relpath):
+                return relpath
+            curpath = self['__dir__']
+            result = os.path.join(curpath, relpath)
+            result = os.path.realpath(result)
+            return result
+
+        def enqueue(pathes):
+            for path in reversed(pathes):
+                path = abspath(path)
+                if os.path.isfile(path) and \
+                   os.path.splitext(path)[1] in self.loaders and \
+                   path not in queue:
+                    queue.appendleft(path)
+                elif os.path.isdir(path):
+                    files = []
+                    for name in os.listdir(path):
+                        subpath = os.path.join(path, name)
+                        if os.path.isfile(subpath):
+                            files.append(subpath)
+                    enqueue(sorted(files))
+
+        enqueue([path])
         while queue:
             path = queue.popleft()
-            self['__loader__.dir'] = os.path.dirname(path)
+            self['__file__'] = path
+            self['__dir__'] = os.path.dirname(path)
             ext = os.path.splitext(path)[1]
             with open(path) as f:
-                data = loaders[ext](f)
+                data = self.loaders[ext](f)
                 self.update(flatten(data))
-            include = self['__loader__'].pop('include', [])
-            self.include(*include)
-
-    def include(self, *pathes):
-        queue = self.setdefault('__loader__.queue', deque())
-        for path in pathes:
-            path = self.abspath(path)
-            if os.path.isfile(path) and \
-               os.path.splitext(path)[1] in loaders and \
-               path not in queue:
-                queue.append(path)
-            elif os.path.isdir(path):
-                files = []
-                for name in os.listdir(path):
-                    subpath = os.path.join(path, name)
-                    if os.path.isfile(subpath):
-                        files.append(subpath)
-                self.include(*sorted(files))
-
-    def abspath(self, relpath):
-        curpath = self.get('__loader__.dir', '.')
-        result = os.path.join(curpath, relpath)
-        result = os.path.realpath(result)
-        return result
+            include = self.pop('__include__', [])
+            if isinstance(include, string):
+                include = [include]
+            enqueue(include)
+            del self['__file__'], self['__dir__']
--- tests/config_test.py
+++ tests/config_test.py
@@ -8,52 +8,34 @@ from configtree.config import ConfigTree
 data_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(data_dir, 'data', 'config')
 
-
-def abspath_test():
-    config = ConfigTree()
-    config['__loader__.dir'] = data_dir
-    path_1 = config.abspath('test')
-    path_2 = config.abspath('../test')
-    tools.eq_(path_1, os.path.realpath(os.path.join(data_dir, 'test')))
-    tools.eq_(path_2, os.path.realpath(os.path.join(data_dir, '../test')))
+config = ConfigTree()
 
 
-def include_test():
-    config = ConfigTree()
-    config['__loader__.dir'] = data_dir
-    config.include('.')
-    tools.eq_(list(config['__loader__.queue']), [
-        os.path.join(data_dir, 'env.json'),
-        os.path.join(data_dir, 'settings.json'),
-    ])
-    config.include('1/env.json')
-    tools.eq_(list(config['__loader__.queue']), [
-        os.path.join(data_dir, 'env.json'),
-        os.path.join(data_dir, 'settings.json'),
-        os.path.join(data_dir, '1', 'env.json'),
-    ])
-    config.include('1')
-    tools.eq_(list(config['__loader__.queue']), [
-        os.path.join(data_dir, 'env.json'),
-        os.path.join(data_dir, 'settings.json'),
-        os.path.join(data_dir, '1', 'env.json'),
-        os.path.join(data_dir, '1', 'settings.json'),
-    ])
-
-
-def load_test():
-    config = ConfigTree()
+def load_dir_test():
     config.load(data_dir)
-    tools.eq_(config['env.name'], 'Common')
-    tools.eq_(config['settings'], {
-        'x': 1,
-        'y': 1,
-        'z': 1,
+    tools.eq_(config, {
+        'env.name': 'Common',
+        'settings.x': 1,
+        'settings.y': 1,
+        'settings.z': 1,
     })
-    config.load('1')
-    tools.eq_(config['env.name'], 'Environment 1')
-    tools.eq_(config['settings'], {
-        'x': 2,
-        'y': 1,
-        'z': 1,
+
+
+def load_file_with_string_include_test():
+    config.load(os.path.join(data_dir, '1', 'env.json'))
+    tools.eq_(config, {
+        'env.name': 'Environment 1',
+        'settings.x': 2,
+        'settings.y': 1,
+        'settings.z': 1,
+    })
+
+
+def load_file_with_list_include_test():
+    config.load(os.path.join(data_dir, '2', 'env.json'))
+    tools.eq_(config, {
+        'env.name': 'Environment 2',
+        'settings.x': 2,
+        'settings.y': 3,
+        'settings.z': 3,
     })
