ec17a4d0af175757d7e89fc72c957bf9107869c1
Dmitry Vakhrushev <self@kr41.net>
2015-06-22 18:14:16 +0600

    Updated public imports

--- configtree/__init__.py
+++ configtree/__init__.py
@@ -1,8 +1,11 @@
 from .tree import Tree, flatten, rarefy
-from .loader import load, make_walk, make_update
+from .loader import load, loaderconf, make_walk, make_update
 
 
-__all__ = ['Tree', 'flatten', 'rarefy', 'load', 'make_walk', 'make_update']
+__all__ = [
+    'Tree', 'flatten', 'rarefy',
+    'load', 'loaderconf', 'make_walk', 'make_update',
+]
 __version__ = '0.2'
 __author__ = 'Dmitry Vakhrushev <self@kr41.net>'
 __license__ = 'BSD'
