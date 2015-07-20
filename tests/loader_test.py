aa97254099123b08c38db6b7fc05e0fe03edb7a6
Dmitry Vakhrushev <self@kr41.net>
2015-07-20 17:22:51 +0600

    Moved deprecated features down within module sources

--- tests/loader_test.py
+++ tests/loader_test.py
@@ -28,162 +28,6 @@ def teardown_func():
         pass
 
 
-def walk_test():
-    walk = make_walk()
-    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
-    tools.eq_(files, [
-        os.path.join('default', 'a.json'),
-        os.path.join('default', 'b.yaml'),
-        os.path.join('default', 'subsystem', 'a.yaml'),
-        os.path.join('default', 'subsystem', 'b.yaml'),
-        os.path.join('final-common', 'c.yaml'),
-        'final-common.yaml',
-    ])
-
-    walk = make_walk(env='y')
-    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
-    tools.eq_(files, [
-        os.path.join('default', 'a.json'),
-        os.path.join('default', 'b.yaml'),
-        os.path.join('default', 'subsystem', 'a.yaml'),
-        os.path.join('default', 'subsystem', 'b.yaml'),
-        'env-y.yaml',
-        os.path.join('final-common', 'c.yaml'),
-        'final-common.yaml',
-    ])
-
-    walk = make_walk(env='x')
-    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
-    tools.eq_(files, [
-        os.path.join('default', 'a.json'),
-        os.path.join('default', 'b.yaml'),
-        os.path.join('default', 'subsystem', 'a.yaml'),
-        os.path.join('default', 'subsystem', 'b.yaml'),
-        os.path.join('env-x', 'a.yaml'),
-        os.path.join('final-common', 'c.yaml'),
-        'final-common.yaml',
-    ])
-
-    walk = make_walk(env='x.xx')
-    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
-    tools.eq_(files, [
-        os.path.join('default', 'a.json'),
-        os.path.join('default', 'b.yaml'),
-        os.path.join('default', 'subsystem', 'a.yaml'),
-        os.path.join('default', 'subsystem', 'b.yaml'),
-        os.path.join('env-x', 'a.yaml'),
-        os.path.join('env-x', 'env-xx', 'b.yaml'),
-        os.path.join('final-common', 'c.yaml'),
-        'final-common.yaml',
-    ])
-
-
-def update_test():
-    tree = Tree({'a.b.x': 1})
-
-    update = make_update()
-    update(tree, 'a.b.y', 2)
-    tools.eq_(tree['a.b.y'], 2)
-
-    update(tree, 'a.b.z', '>>> self["a.b.x"] + branch["y"]')
-    tools.eq_(tree['a.b.z'], 3)
-
-    update(tree, 'a.b.l?', [])
-    tools.eq_(tree['a.b.l'], [])
-
-    update(tree, 'a.b.l?', [1, 2, 3])
-    tools.eq_(tree['a.b.l'], [])
-
-    update(tree, 'a.b.l#extend', [4, 5, 6])
-    tools.eq_(tree['a.b.l'], [4, 5, 6])
-
-    update(tree, 'a.b.c', '$>> x = {self[a.b.x]}, y = {branch[y]}')
-    tools.eq_(tree['a.b.c'], 'x = 1, y = 2')
-
-    update = make_update(namespace={'floor': math.floor})
-    update(tree, 'z', '>>> int(floor(3.8))')
-    tools.eq_(tree['z'], 3)
-
-
-def load_test():
-    update = make_update(namespace={'floor': math.floor})
-    result = load(data_dir, update=update)
-    tools.eq_(result, {
-        'a.x': 1,
-        'a.y': 2,
-        'b.x': 10,
-        'b.y': 20,
-        'subsystem.a.x': 101,
-        'subsystem.a.y': 102,
-        'subsystem.b.x': 110,
-        'subsystem.b.y': 120,
-        'c.x': 10,
-        'c.y': 20,
-        'c.z': 30,
-    })
-
-    walk = make_walk(env='y')
-    result = load(data_dir, walk=walk, update=update)
-    tools.eq_(result, {
-        'a.x': 1,
-        'a.y': 2,
-        'b.x': 10,
-        'b.y': 20,
-        'subsystem.a.x': 101,
-        'subsystem.a.y': 102,
-        'subsystem.b.x': 110,
-        'subsystem.b.y': 120,
-        'a.b.x': 1,
-        'a.b.y': 2,
-        'a.b.z': 3,
-        'a.b.c': 'x = 1, y = 2',
-        'a.b.l': [4, 5, 6],
-        'z': 3,
-        'path': os.path.join(data_dir, 'somepath'),
-        'here': os.path.join(data_dir, 'env-y.yaml'),
-        'c.x': 10,
-        'c.y': 20,
-        'c.z': 30,
-    })
-
-
-def postprocess_test():
-
-    def postprocess(tree):
-        for key, value in tree.items():
-            tree[key] = value + 1000
-
-    update = make_update(namespace={'floor': math.floor})
-    result = load(data_dir, update=update, postprocess=postprocess)
-    tools.eq_(result, {
-        'a.x': 1001,
-        'a.y': 1002,
-        'b.x': 1010,
-        'b.y': 1020,
-        'subsystem.a.x': 1101,
-        'subsystem.a.y': 1102,
-        'subsystem.b.x': 1110,
-        'subsystem.b.y': 1120,
-        'c.x': 1010,
-        'c.y': 1020,
-        'c.z': 1030,
-    })
-
-
-@tools.with_setup(teardown=teardown_func)
-def loader_conf_test():
-    conf = loaderconf(os.path.dirname(data_dir))
-    tools.eq_(conf, {})
-
-    conf = loaderconf(data_dir)
-    tools.eq_(conf, {
-        'walk': 'walk',
-        'update': 'update',
-        'postprocess': 'postprocess',
-        'tree': 'tree',
-    })
-
-
 def pipeline_test():
 
     class Test(Pipeline):
@@ -384,3 +228,164 @@ def postprocessor_check_required_test():
         postprocess(tree)
 
     tools.eq_(context.exception.args[0], [tree['bar'], tree['foo']])
+
+
+###############################################################################
+# Deprectated features tests
+##
+
+
+def walk_test():
+    walk = make_walk()
+    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
+    tools.eq_(files, [
+        os.path.join('default', 'a.json'),
+        os.path.join('default', 'b.yaml'),
+        os.path.join('default', 'subsystem', 'a.yaml'),
+        os.path.join('default', 'subsystem', 'b.yaml'),
+        os.path.join('final-common', 'c.yaml'),
+        'final-common.yaml',
+    ])
+
+    walk = make_walk(env='y')
+    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
+    tools.eq_(files, [
+        os.path.join('default', 'a.json'),
+        os.path.join('default', 'b.yaml'),
+        os.path.join('default', 'subsystem', 'a.yaml'),
+        os.path.join('default', 'subsystem', 'b.yaml'),
+        'env-y.yaml',
+        os.path.join('final-common', 'c.yaml'),
+        'final-common.yaml',
+    ])
+
+    walk = make_walk(env='x')
+    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
+    tools.eq_(files, [
+        os.path.join('default', 'a.json'),
+        os.path.join('default', 'b.yaml'),
+        os.path.join('default', 'subsystem', 'a.yaml'),
+        os.path.join('default', 'subsystem', 'b.yaml'),
+        os.path.join('env-x', 'a.yaml'),
+        os.path.join('final-common', 'c.yaml'),
+        'final-common.yaml',
+    ])
+
+    walk = make_walk(env='x.xx')
+    files = [os.path.relpath(f, data_dir) for f in walk(data_dir)]
+    tools.eq_(files, [
+        os.path.join('default', 'a.json'),
+        os.path.join('default', 'b.yaml'),
+        os.path.join('default', 'subsystem', 'a.yaml'),
+        os.path.join('default', 'subsystem', 'b.yaml'),
+        os.path.join('env-x', 'a.yaml'),
+        os.path.join('env-x', 'env-xx', 'b.yaml'),
+        os.path.join('final-common', 'c.yaml'),
+        'final-common.yaml',
+    ])
+
+
+def update_test():
+    tree = Tree({'a.b.x': 1})
+
+    update = make_update()
+    update(tree, 'a.b.y', 2)
+    tools.eq_(tree['a.b.y'], 2)
+
+    update(tree, 'a.b.z', '>>> self["a.b.x"] + branch["y"]')
+    tools.eq_(tree['a.b.z'], 3)
+
+    update(tree, 'a.b.l?', [])
+    tools.eq_(tree['a.b.l'], [])
+
+    update(tree, 'a.b.l?', [1, 2, 3])
+    tools.eq_(tree['a.b.l'], [])
+
+    update(tree, 'a.b.l#extend', [4, 5, 6])
+    tools.eq_(tree['a.b.l'], [4, 5, 6])
+
+    update(tree, 'a.b.c', '$>> x = {self[a.b.x]}, y = {branch[y]}')
+    tools.eq_(tree['a.b.c'], 'x = 1, y = 2')
+
+    update = make_update(namespace={'floor': math.floor})
+    update(tree, 'z', '>>> int(floor(3.8))')
+    tools.eq_(tree['z'], 3)
+
+
+def load_test():
+    update = make_update(namespace={'floor': math.floor})
+    result = load(data_dir, update=update)
+    tools.eq_(result, {
+        'a.x': 1,
+        'a.y': 2,
+        'b.x': 10,
+        'b.y': 20,
+        'subsystem.a.x': 101,
+        'subsystem.a.y': 102,
+        'subsystem.b.x': 110,
+        'subsystem.b.y': 120,
+        'c.x': 10,
+        'c.y': 20,
+        'c.z': 30,
+    })
+
+    walk = make_walk(env='y')
+    result = load(data_dir, walk=walk, update=update)
+    tools.eq_(result, {
+        'a.x': 1,
+        'a.y': 2,
+        'b.x': 10,
+        'b.y': 20,
+        'subsystem.a.x': 101,
+        'subsystem.a.y': 102,
+        'subsystem.b.x': 110,
+        'subsystem.b.y': 120,
+        'a.b.x': 1,
+        'a.b.y': 2,
+        'a.b.z': 3,
+        'a.b.c': 'x = 1, y = 2',
+        'a.b.l': [4, 5, 6],
+        'z': 3,
+        'path': os.path.join(data_dir, 'somepath'),
+        'here': os.path.join(data_dir, 'env-y.yaml'),
+        'c.x': 10,
+        'c.y': 20,
+        'c.z': 30,
+    })
+
+
+def postprocess_test():
+
+    def postprocess(tree):
+        for key, value in tree.items():
+            tree[key] = value + 1000
+
+    update = make_update(namespace={'floor': math.floor})
+    result = load(data_dir, update=update, postprocess=postprocess)
+    tools.eq_(result, {
+        'a.x': 1001,
+        'a.y': 1002,
+        'b.x': 1010,
+        'b.y': 1020,
+        'subsystem.a.x': 1101,
+        'subsystem.a.y': 1102,
+        'subsystem.b.x': 1110,
+        'subsystem.b.y': 1120,
+        'c.x': 1010,
+        'c.y': 1020,
+        'c.z': 1030,
+    })
+
+
+@tools.with_setup(teardown=teardown_func)
+def loader_conf_test():
+    conf = loaderconf(os.path.dirname(data_dir))
+    tools.eq_(conf, {})
+
+    conf = loaderconf(data_dir)
+    tools.eq_(conf, {
+        'walk': 'walk',
+        'update': 'update',
+        'postprocess': 'postprocess',
+        'tree': 'tree',
+    })
